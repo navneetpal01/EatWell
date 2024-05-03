a15f24950d28aa203440f968a8ae0b78a27f56c6
navneet <navneetpalgithub@gmail.com>
2024-05-03 19:57:33 +0530

    24

--- app/src/main/java/com/example/eatwell/presentation/onboarding/OnBoardingScreen.kt
+++ app/src/main/java/com/example/eatwell/presentation/onboarding/OnBoardingScreen.kt
@@ -1,30 +1,59 @@
 package com.example.eatwell.presentation.onboarding
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.example.eatwell.R
 
 
 @Composable
 fun OnBoardingScreen(
-    content : @Composable (PaddingValues) -> Unit
-){
+) {
     Scaffold(
         modifier = Modifier
             .fillMaxSize(),
         topBar = {
+            //TODO Top App Bar
 
         },
-        content = content,
+        content = { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+            ) {
+
+            }
+        },
         bottomBar = {
+            //TODO Bottom App Bar
 
         }
     )
 
+}
+
+@Composable
+fun TopAppBarTittle(
+    pageIndex : Int,
+    totalPages : Int,
+    modifier: Modifier = Modifier
+){
+    Row(
+        modifier = modifier
+    ){
+        Text(
+            text = (pageIndex + 1).toString(),
+            
+        )
+        Text(
+            text = ""
+        )
+    }
 }
